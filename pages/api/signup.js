5fca58ee2c34c57bfaffcc7f5097a8fbcb426f05
Julia_Johnson <julialj95@gmail.com>
2022-12-03 22:42:45 -0600

    full auth added

--- pages/api/signup.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import cookie from 'cookie';
-import prisma from '../../lib/prisma';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    
-}
---
+++ pages/api/signup.ts
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import prisma from '../../lib/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const validateRoute = (handler) => {
+    return async (req: NextApiRequest, res: NextApiResponse) => {
+      const token = req.cookies.TRAX_ACCESS_TOKEN
+  
+      if (token) {
+        let user
+  
+        try {
+          const { id } = jwt.verify(token, 'hello')
+          user = await prisma.user.findUnique({
+            where: { id },
+          })
+  
+          if (!user) {
+            throw new Error('Not real user')
+          }
+        } catch (error) {
+          res.status(401)
+          res.json({ error: 'Not Authorizied' })
+          return
+        }
+  
+        return handler(req, res, user)
+      }
+  
+      res.status(401)
+      res.json({ error: 'Not Authorizied' })
+    }
+  }
+  
+  export const validateToken = (token) => {
+    const user = jwt.verify(token, 'hello')
+    return user
+  }
